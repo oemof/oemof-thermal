7bb5b870ecee242201c9beb900f128f15bfcbacf
jnnr <32454596+jnnr@users.noreply.github.com>
2019-10-24 11:57:14 +0200

    Add formulas to docstrings

--- src/oemof/thermal/stratified_thermal_storage.py
+++ src/oemof/thermal/stratified_thermal_storage.py
@@ -16,6 +16,10 @@ def calculate_storage_u_value(s_iso, lamb_iso, alpha_inside, alpha_outside):
     r"""
     Calculates the thermal transmittance (U-value) of a thermal storage.
 
+    .. calculate_storage_u_value-equations:
+
+    :math:`U = \frac{1}{\frac{1}{\alpha_i} + \frac{s_{iso}}{\lambda_ {iso}} + \frac{1}{\alpha_a}}`
+
     Parameters
     ----------
     s_iso : numeric
@@ -38,6 +42,14 @@ def calculate_capacities(height, diameter, temp_h, temp_c, nonusable_storage_vol
     r"""
     Calculates the nominal storage capacity, surface area, minimum
     and maximum storage level of a stratified thermal storage.
+
+    .. calculate_capacities-equations:
+
+    :math:`Q_{max} = Q_N \cdot (1-\beta/2)`
+
+    :math:`Q_{min} = Q_N \cdot \beta/2`
+
+    :math:`Q_N = \frac{d^2}{4} \cdot \pi \cdot h \cdot c \cdot \rho \cdot \left( T_{H} - T_{C} \right)`
 
     Parameters
     ----------
@@ -68,6 +80,11 @@ def calculate_losses(nominal_storage_capacity, u_value, surface, temp_h, temp_c,
     r"""
     Calculates loss rate and fixed losses for a stratified thermal storage.
 
+    .. calculate_losses-equations:
+
+    \beta =  U \cdot A \cdot \DeltaT_{HC} / Q_N
+    \gamma = U \cdot A \cdot \DeltaT_{C0} / Q_N
+
     Parameters
     ----------
     nominal_storage_capacity : numeric
@@ -82,7 +99,7 @@ def calculate_losses(nominal_storage_capacity, u_value, surface, temp_h, temp_c,
     loss_rate : numeric
     fixed_losses : numeric
     """
-    loss_rate = 1/nominal_storage_capacity * u_value * surface * (temp_h - temp_c)
+    loss_rate =    u_value * surface * (temp_h - temp_c)   * 1 / nominal_storage_capacity
     fixed_losses = u_value * surface * (temp_c - temp_env) * 1 / nominal_storage_capacity
 
     return loss_rate, fixed_losses
