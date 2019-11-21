4cb821978263a5df5ced7fbf777e93d628786e66
FranziPl <franziska.pleissner@beuth-hochschule.de>
2019-11-21 16:06:45 +0100

    Fix style of docstring and variable names

--- src/oemof/thermal/CSP.py
+++ src/oemof/thermal/CSP.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 def csp_precalc(df, periods,
                 lat, long, tz,
-                collector_tilt, collector_azimuth, x, a1, a2,
+                collector_tilt, collector_azimuth, x, a_1, a_2,
                 eta_0, c_1, c_2,
-                collector_inlet_temp, collector_outlet_temp,
+                temp_collector_inlet, temp_collector_outlet,
                 irradiance_method='horizontal',
                 date_col='date', irradiance_col='E_dir_hor',
-                t_amb_col='t_amb'):
+                temp_amb_col='t_amb'):
     """
     Calculates collectors efficiency and irradiance according to [1] and the
     heat of the thermal collector
@@ -24,42 +24,42 @@ def csp_precalc(df, periods,
     periods: numeric
         Defines the number of timesteps.
     lat: numeric
-       Latitude of the location.
+        Latitude of the location.
     long: numeric
-        longitude of the location
+        Longitude of the location.
     tz: string
-        pytz timezone of the location
+        pytz timezone of the location.
     collector_tilt: numeric
-        the tilt of the collector.
+        The tilt of the collector.
     collector_azimuth: numeric
-            the azimuth of the collector. Azimuth according to pvlib in decimal
-            degrees East of North
+        The azimuth of the collector. Azimuth according to pvlib in decimal
+        degrees East of North
     x: numeric
-        Cleanliness of the collector (between 0 and 1)
-    a1: numeric
-        parameter for the incident angle modifier
-    a2: numeric
-        parameter for the incident angle modifier
+        Cleanliness of the collector (between 0 and 1).
+    a_1: numeric
+        Parameter for the incident angle modifier.
+    a_2: numeric
+        Parameter for the incident angle modifier.
     eta_0: numeric
-        optical efficiency of the collector
+        Optical efficiency of the collector.
     c_1: numeric
-        thermal loss parameter
+        Thermal loss parameter 1.
     c_2: numeric
-        thermal loss parameter
-    collector_inlet_temp: numeric or series with length periods
-            collectors inlet temperature
-    collector_outlet_temp: numeric or series with length periods
-            collectors outlet temperature
+        Thermal loss parameter 2.
+    temp_collector_inlet: numeric or series with length periods
+        Collectors inlet temperature.
+    temp_collector_outlet: numeric or series with length periods
+        Collectors outlet temperature.
     irradiance_method: string, default 'horizontal'
-            valid values are: 'horizontal' or 'normal'. Describes, if the
-            horizontal direct irradiance or the direct normal irradiance is
-            given and used for calculation
+        Valid values are: 'horizontal' or 'normal'. Describes, if the
+        horizontal direct irradiance or the direct normal irradiance is
+        given and used for calculation.
     date_col: string, default: 'date'
-            describes the name of the column in the dataframe df
+        Describes the name of the column in the dataframe df.
     irradiance_col: string, default: 'E_dir_hor'
-            describes the name of the column in the dataframe df
-    t_amb_col: string, default: 't_amb_col'
-            describes the name of the column in the dataframe df
+        Describes the name of the column in the dataframe df.
+    temp_amb_col: string, default: 't_amb_col'
+        Describes the name of the column in the dataframe df.
 
     Returns
     -------
@@ -69,8 +69,8 @@ def csp_precalc(df, periods,
         * collector_heat
 
     collector_irradiance:
-        the irradiance on collector after all losses which \
-        occur before the light hits the collectors surface
+        The irradiance on collector after all losses which \
+        occur before the light hits the collectors surface.
 
 
     **proposal of values**
@@ -78,7 +78,7 @@ def csp_precalc(df, periods,
     If you have no idea, which values your collector have, here are values,
     which were measured in [1] for a collector:
     a1: -0.00159, a2: 0.0000977,
-    eta_0: 0.816, c1: 0.0622, c2: 0.00023,
+    eta_0: 0.816, c1: 0.0622, c2: 0.00023.
 
     **Reference**
 
@@ -96,11 +96,11 @@ def csp_precalc(df, periods,
     if irradiance_method == 'horizontal':
         data = pd.DataFrame({'date': date_time_index,
                              'E_dir_hor': datainput[irradiance_col],
-                             't_amb': datainput[t_amb_col]})
+                             't_amb': datainput[temp_amb_col]})
     elif irradiance_method == 'normal':
         data = pd.DataFrame({'date': date_time_index,
                              'dni': datainput[irradiance_col],
-                             't_amb': datainput[t_amb_col]})
+                             't_amb': datainput[temp_amb_col]})
     else:
         raise AttributeError("irradiance_method must be 'horizontal' or"
                              "'normal'")
@@ -139,11 +139,11 @@ def csp_precalc(df, periods,
     data['collector_irradiance'] = collector_irradiance
 
     # Calculation of the incidence angle modifier
-    iam = calc_iam(a1, a2, tracking_data['aoi'])
+    iam = calc_iam(a_1, a_2, tracking_data['aoi'])
 
     # Calculation of the collectors efficiency
     eta_c = calc_eta_c(eta_0, c_1, c_2, iam,
-                       collector_inlet_temp, collector_outlet_temp,
+                       temp_collector_inlet, temp_collector_outlet,
                        data['t_amb'], collector_irradiance)
     data['eta_c'] = eta_c
 
@@ -161,44 +161,44 @@ def calc_collector_irradiance(irradiance_on_collector, x):
     Parameters
     ----------
     irradiance_on_collector: series of numeric
-        irradiance which hits collectors surface
+        Irradiance which hits collectors surface.
     x: numeric
-        Cleanliness of the collector (between 0 and 1)
+        Cleanliness of the collector (between 0 and 1).
 
     Returns
     -------
     collector_irradiance: series of numeric
-        irradiance on collector after all losses
+        Irradiance on collector after all losses.
 
     """
     collector_irradiance = irradiance_on_collector * x**1.5
     return collector_irradiance
 
 
-def calc_iam(a1, a2, aoi):
+def calc_iam(a_1, a_2, aoi):
     """
     Calculates the incidence angle modifier
 
     Parameters
     ----------
-    a1: numeric
-        parameter 1 for the incident angle modifier
-    a2: numeric
-        parameter 2 for the incident angle modifier
+    a_1: numeric
+        Parameter 1 for the incident angle modifier.
+    a_2: numeric
+        Parameter 2 for the incident angle modifier.
     aoi: series of numeric
-        angle of incidence
+        Angle of incidence.
 
     Returns
     -------
-    incidence angle modifier: series of numeric
+    Iicidence angle modifier: series of numeric
 
     """
-    iam = 1 - a1 * abs(aoi) - a2 * aoi**2
+    iam = 1 - a_1 * abs(aoi) - a_2 * aoi**2
     return iam
 
 
 def calc_eta_c(eta_0, c_1, c_2, iam,
-               collector_inlet_temp, collector_outlet_temp, t_amb,
+               temp_collector_inlet, temp_collector_outlet, temp_amb,
                collector_irradiance):
     """
     Calculates collectors efficiency
@@ -206,28 +206,28 @@ def calc_eta_c(eta_0, c_1, c_2, iam,
     Parameters
     ----------
     eta_0: numeric
-        optical efficiency of the collector
+        Optical efficiency of the collector.
     c_1: numeric
-        thermal loss parameter
+        Thermal loss parameter 1.
     c_2: numeric
-        thermal loss parameter
+        Thermal loss parameter 2.
     iam: series of numeric
-        incidence angle modifier
-    collector_inlet_temp: numeric, in °C
-        collectors inlet temperature
-    collector_outlet_temp: mumeric, in °C
-        collectors outlet temperature
-    t_amb: series of numeric, in °C
-        ambient temperature
+        Incidence angle modifier.
+    temp_collector_inlet: numeric, in °C
+        Collectors inlet temperature.
+    temp_collector_outlet: mumeric, in °C
+        Collectors outlet temperature.
+    temp_amb: series of numeric, in °C
+        Ambient temperature.
     collector_irradiance: series of numeric
-        irradiance on collector after all losses
+        Irradiance on collector after all losses.
 
     Returns
     -------
     collectors efficiency: series of numeric
 
     """
-    delta_t = (collector_inlet_temp + collector_outlet_temp) / 2 - t_amb
+    delta_t = (temp_collector_inlet + temp_collector_outlet) / 2 - temp_amb
     eta_c = pd.Series()
     for index, value in collector_irradiance.items():
         if value > 0:
