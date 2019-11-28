c43bbeabe816c6bea5ee5639ab696ca85bab2731
jnnr <jann.launer@rl-institut.de>
2019-11-28 21:16:36 +0100

    Allow to compare different storages

--- examples/stratified_thermal_storage/plots.py
+++ examples/stratified_thermal_storage/plots.py
@@ -2,12 +2,13 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from oemof.thermal.stratified_thermal_storage import (calculate_storage_u_value,
-                                                      calculate_storage_dimensions,
-                                                      calculate_capacities,
-                                                      calculate_losses)
-from oemof.solph import (Bus, Flow,
-                         Model, EnergySystem)
+from oemof.thermal.stratified_thermal_storage import (
+    calculate_storage_u_value,
+    calculate_storage_dimensions,
+    calculate_capacities,
+    calculate_losses,
+)
+from oemof.solph import Bus, Flow, Model, EnergySystem
 from oemof.solph.components import GenericStorage
 import oemof.outputlib as outputlib
 
@@ -22,7 +23,8 @@ u_value = calculate_storage_u_value(
     input_data['s_iso'],
     input_data['lamb_iso'],
     input_data['alpha_inside'],
-    input_data['alpha_outside'])
+    input_data['alpha_outside']
+)
 
 volume, surface = calculate_storage_dimensions(
     input_data['height'],
@@ -35,14 +37,16 @@ nominal_storage_capacity, max_storage_level, min_storage_level = calculate_capac
     input_data['temp_c'],
     input_data['nonusable_storage_volume'],
     input_data['heat_capacity'],
-    input_data['density'])
+    input_data['density']
+)
 
 loss_rate, fixed_losses_relative, fixed_losses_absolute = calculate_losses(
     u_value,
     input_data['diameter'],
     input_data['temp_h'],
     input_data['temp_c'],
-    input_data['temp_env'])
+    input_data['temp_env'],
+)
 
 maximum_heat_flow_charging = 2
 maximum_heat_flow_discharging = 2
@@ -86,8 +90,10 @@ energysystem = EnergySystem(timeindex=datetimeindex)
 
 bus_heat = Bus(label='bus_heat')
 
-thermal_storage_1 = GenericStorage(
-    label='thermal_storage_1',
+storage_list = []
+
+storage_list.append(GenericStorage(
+    label='thermal_storage_0',
     inputs={bus_heat: Flow(
         nominal_value=maximum_heat_flow_charging,
         variable_costs=0.0001)},
@@ -101,28 +107,30 @@ thermal_storage_1 = GenericStorage(
     inflow_conversion_factor=1.,
     outflow_conversion_factor=1.,
     balanced=False
-)
+))
 
-thermal_storage_2 = GenericStorage(
-    label='thermal_storage_2',
-    inputs={bus_heat: Flow(
-        nominal_value=maximum_heat_flow_charging,
-        variable_costs=0.0001)},
-    outputs={bus_heat: Flow(
-        nominal_value=maximum_heat_flow_discharging)},
-    nominal_storage_capacity=nominal_storage_capacity,
-    min_storage_level=min_storage_level,
-    max_storage_level=max_storage_level,
-    initial_storage_level=0.9,
-    loss_rate=loss_rate,
-    fixed_losses_relative=fixed_losses_relative,
-    fixed_losses_absolute=fixed_losses_absolute,
-    inflow_conversion_factor=1.,
-    outflow_conversion_factor=1.,
-    balanced=False
-)
+for i, nominal_storage_capacity in enumerate([3,5,10]):
+    print(i)
+    storage_list.append(GenericStorage(
+        label=f'thermal_storage_{i+1}',
+        inputs={bus_heat: Flow(
+            nominal_value=maximum_heat_flow_charging,
+            variable_costs=0.0001)},
+        outputs={bus_heat: Flow(
+            nominal_value=maximum_heat_flow_discharging)},
+        nominal_storage_capacity=nominal_storage_capacity,
+        min_storage_level=min_storage_level,
+        max_storage_level=max_storage_level,
+        initial_storage_level=0.9,
+        loss_rate=loss_rate,
+        fixed_losses_relative=fixed_losses_relative,
+        fixed_losses_absolute=fixed_losses_absolute,
+        inflow_conversion_factor=1.,
+        outflow_conversion_factor=1.,
+        balanced=False
+    ))
 
-energysystem.add(bus_heat, thermal_storage_1, thermal_storage_2)
+energysystem.add(bus_heat, *storage_list)
 
 # create and solve the optimization model
 optimization_model = Model(energysystem)
@@ -130,32 +138,35 @@ optimization_model.solve(solver=solver)
 
 # get results
 results = outputlib.processing.results(optimization_model)
-string_results = outputlib.processing.convert_keys_to_strings(results)
-sequences = {k: v['sequences'] for k, v in string_results.items()}
-df = pd.concat(sequences, axis=1)
 
-storage_content = df.loc[:, [('thermal_storage_1', 'None', 'capacity'),
-                             ('thermal_storage_2', 'None', 'capacity')]]
+storage_content = outputlib.views.node_weight_by_type(results, GenericStorage).reset_index(drop=True)
 
-losses = - storage_content.iloc[1:, :].values + storage_content.iloc[:-1, :].values
+storage_content.columns = storage_content.columns.set_levels([k.label for k in storage_content.columns.levels[0]], level=0)
+
+losses = - storage_content.iloc[1:, :].reset_index(drop=True) + storage_content.iloc[:-1, :].reset_index(drop=True)
+
+losses.columns = losses.columns.set_levels(['losses'], level=1)
+
+storage_content = storage_content.iloc[:-1, :]
+
+storage_df = pd.concat([storage_content, losses], 1)
+
+storage_df = storage_df.reindex(sorted(storage_df.columns), axis=1)
 
 # plot storage_content vs. time
 fig, ax = plt.subplots(figsize=(8, 5))
-df[('thermal_storage_1', 'None', 'capacity')].plot(ax=ax)
-df[('thermal_storage_2', 'None', 'capacity')].plot(ax=ax)
+storage_content.plot(ax=ax)
 ax.set_title('Storage content')
 ax.set_xlabel('Timesteps')
 ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
 plt.tight_layout()
 plt.show()
 
-#   losses vs storage content
+# plot losses vs storage content
 fig, ax = plt.subplots()
-plt.scatter(storage_content[1:], losses)
+for storage in (storage.label for storage in storage_list):
+    ax.scatter(storage_df[(storage, 'capacity')], storage_df[(storage, 'losses')])
 ax.set_title('Losses vs. storage content')
 ax.set_xlabel('Storage content [MWh]')
 ax.set_ylabel('Losses [MWh]')
 plt.show()
-
-# TODO: losses vs capacity
-# TODO: cost function (overnight cost per capacity)
