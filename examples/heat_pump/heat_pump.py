bf3491419134cff5878dac0a5b54dd4348810627
Pyosch <birk-sascha@web.de>
2019-12-06 13:02:13 +0100

    added warnings and example

---
+++ examples/heat_pump/heat_pump.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+author: pyosch
+"""
+import pandas as pd
+import numpy as np
+import heat_pump
+
+# generate test data
+temperature = pd.DataFrame(np.random.randint(-10, 20, size=(96, 1)),
+                           columns=["temperature"],
+                           index=pd.date_range(start='2017', periods=96,
+                                               freq='15 min', name='index'))
+
+print(temperature.head())
+
+# cop calculation for heat pumps
+cop = heat_pump.get_cop(temperature.temperature)
+
+print(cop.head())
