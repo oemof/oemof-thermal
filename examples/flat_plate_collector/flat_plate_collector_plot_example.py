16d110c7b2e3ff104f3b6a5356124f63f1de3836
caroline.moeller <caroline.moeller@rl-institut.de>
2019-12-16 14:51:15 +0100

    Adapt code to Pep8 style

--- examples/flat_plate_collector/flat_plate_collector_plot_example.py
+++ examples/flat_plate_collector/flat_plate_collector_plot_example.py
@@ -13,18 +13,19 @@ def plot_collector_heat(data_precalc, periods, eta_0):
     heat_calc = data_precalc['collectors_heat']
     irradiance_on_collector = data_precalc['col_ira']
     heat_compare = irradiance_on_collector * eta_0
-    t = list(range(1, periods+1))
-
+    t = list(range(1, periods + 1))
 
     fig, ax = plt.subplots()
     ax.plot(t, heat_calc, label='CSP precalculation')
     ax.plot(t, heat_compare, label='constant efficiency')
-    ax.set(xlabel='time in h', ylabel='Q_coll in kWh',
-           title='Heat of the collector')
+    ax.set(
+        xlabel='time in h',
+        ylabel='Q_coll in kWh',
+        title='Heat of the collector',
+    )
     ax.grid()
     ax.legend()
     plt.show()
     plt.savefig('compare_precalculations.png')
 
     return
-
