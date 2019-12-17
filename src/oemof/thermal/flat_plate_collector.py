fae1a0eae821796d5ad073406e08de7bdccc1bb3
Caroline Möller <caroline.moeller@rl-institut.de>
2019-12-17 14:04:34 +0100

    Update src/oemof/thermal/flat_plate_collector.py
    Co-Authored-By: FranziPl <35954135+FranziPl@users.noreply.github.com>

--- src/oemof/thermal/flat_plate_collector.py
+++ src/oemof/thermal/flat_plate_collector.py
@@ -150,7 +150,9 @@ def calc_eta_c_flate_plate(
 
     :math:`\eta_C = \eta_0 - c_1 \cdot \frac{\Delta T}{E_{coll}}
     - c_2 \cdot \frac{{\Delta T}^2}{E_{coll}}`
+
     with
+    
     :math:`\Delta T = T_{collector in} + {\Delta T}_n - T_{amb}`
 
     Parameters
