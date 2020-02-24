7b238a29e4bda5afe9465a878ba475935d81f050
jnnr <jann.launer@rl-institut.de>
2020-02-24 11:39:54 +0100

    Update tests after fixing default values

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -39,7 +39,7 @@ def test_calculate_capacities():
     }
 
     nominal_storage_capacity, max_storage_level, min_storage_level = calculate_capacities(**params)
-    assert nominal_storage_capacity == 56.62268888888889\
+    assert nominal_storage_capacity == 56.62804059111111\
         and max_storage_level == 0.95\
         and min_storage_level == 0.05
 
@@ -54,8 +54,8 @@ def test_calculate_losses():
     }
 
     loss_rate, fixed_losses_relative, fixed_losses_absolute = calculate_losses(**params)
-    assert loss_rate == 0.00035450164405061065\
-        and fixed_losses_relative == 0.00028360131524048847\
+    assert loss_rate == 0.0003531819182021882\
+        and fixed_losses_relative == 0.00028254553456175054\
         and fixed_losses_absolute == 0.010210176124166827
 
 
