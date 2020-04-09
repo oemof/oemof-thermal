a7803f8b676d493aba66593f18f92a8a70a1e5df
jnnr <jann.launer@rl-institut.de>
2020-04-09 16:36:23 +0200

    Print results for charging capacity

--- examples/stratified_thermal_storage/stratified_thermal_storage_investment_option_1.py
+++ examples/stratified_thermal_storage/stratified_thermal_storage_investment_option_1.py
@@ -133,9 +133,10 @@ df = pd.concat(sequences, axis=1)
 # print storage sizing
 built_storage_capacity = results[thermal_storage, None]['scalars']['invest']
 initial_storage_capacity = results[thermal_storage, None]['scalars']['init_cap']
-maximum_heat_flow_charging = results[bus_heat, thermal_storage]['scalars']
+maximum_heat_flow_charging = results[bus_heat, thermal_storage]['scalars']['invest']
 
 dash = '-' * 50
 print(dash)
-print('{:>32s}{:>15.3f}'.format('Built storage capacity [MWh]', built_storage_capacity))
+print('{:>32s}{:>15.3f}'.format('Invested capacity [MW]', maximum_heat_flow_charging))
+print('{:>32s}{:>15.3f}'.format('Invested storage capacity [MWh]', built_storage_capacity))
 print(dash)
