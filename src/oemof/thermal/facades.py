25cdfc9e56e4398f7ce792ad1e01920e5f051dcb
jnnr <jann.launer@rl-institut.de>
2020-04-20 18:05:23 +0200

    Update class name in example

--- src/oemof/thermal/facades.py
+++ src/oemof/thermal/facades.py
@@ -351,10 +351,10 @@ class ParabolicTroughCollector(Transformer, Facade):
     Examples
     --------
     >>> from oemof import solph
-    >>> from oemof.thermal.facades import Collector
+    >>> from oemof.thermal.facades import ParabolicTroughCollector
     >>> bth = solph.Bus(label='thermal_bus')
     >>> bel = solph.Bus(label='electrical_bus')
-    >>> collector = Collector(
+    >>> collector = ParabolicTroughCollector(
     ...     label='solar_collector',
     ...     heat_bus=bth,
     ...     electrical_bus=bel,
