88b616f0c7444252a3f4b63e538b36542fdb6e97
Jakob Wolf <jakob.wolf@beuth-hochschule.de>
2020-04-23 13:45:21 +0200

    Improve example code

--- examples/absorption_heatpumps_and_chiller/cooling_cap_dependence_on_cooling_water_temp.py
+++ examples/absorption_heatpumps_and_chiller/cooling_cap_dependence_on_cooling_water_temp.py
@@ -10,12 +10,11 @@ charpara = pd.read_csv(filename)
 chiller_name = 'Kuehn'
 
 t_cooling = [23, 25, 27, 29, 31, 33, 35, 36, 37, 38, 39, 40]
-n = len(t_cooling)
 
 ddt_75 = abs_hp_chiller.calc_characteristic_temp(
-    t_hot=[75] * n,
+    t_hot=[75],
     t_cool=t_cooling,
-    t_chill=[15] * n,
+    t_chill=[15],
     coef_a=charpara[(charpara['name'] == chiller_name)]['a'].values[0],
     coef_e=charpara[(charpara['name'] == chiller_name)]['e'].values[0],
     method='kuehn_and_ziegler')
@@ -32,9 +31,9 @@ Q_dots_gen_75 = abs_hp_chiller.calc_heat_flux(
 COPs_75 = [Qevap / Qgen for Qgen, Qevap in zip(Q_dots_gen_75, Q_dots_evap_75)]
 
 ddt_80 = abs_hp_chiller.calc_characteristic_temp(
-    t_hot=[80] * n,
+    t_hot=[80],
     t_cool=t_cooling,
-    t_chill=[15] * n,
+    t_chill=[15],
     coef_a=charpara[(charpara['name'] == chiller_name)]['a'].values[0],
     coef_e=charpara[(charpara['name'] == chiller_name)]['e'].values[0],
     method='kuehn_and_ziegler')
@@ -49,16 +48,16 @@ fig1 = plt.figure()
 fig1.set_size_inches(8, 6, forward=True)
 ax1 = fig1.add_subplot(111)
 ax1.grid(axis='y')
-# ax1.grid()
+
 line1 = ax1.plot(t_cooling,
                  Q_dots_evap_80,
-                 linestyle='--',
+                 linestyle='dotted',
                  marker='d',
                  color='black',
                  label='Cooling capacity ($80°$C driving heat)')
 line2 = ax1.plot(t_cooling,
                  Q_dots_evap_75,
-                 linestyle='--',
+                 linestyle='dashed',
                  marker='d',
                  color='black',
                  label='Cooling capacity ($75°$C driving heat)')
