281f8cff165ef24e0ea123e9a1e0e7e5a4db9624
Marie Gering <marie-claire.gering@rl-institut.de>
2020-05-05 17:49:59 +0200

    Remove saving of figure in csp_collector_plot_example_andasol.py

--- examples/concentrating_solar_power/csp_collector_plot_example_andasol.py
+++ examples/concentrating_solar_power/csp_collector_plot_example_andasol.py
@@ -77,7 +77,6 @@ ax.set(xlabel='time (h)', ylabel='Q_coll [W/m2]',
        title='Heat of the collector')
 ax.grid()
 ax.legend()
-plt.savefig('results/compare_collector_heat_method2.png')
 
 # plot showing the difference between a constant efficiency and the efficiency
 # depending on the ambient temperature for the same irradiance and hour of the
@@ -107,4 +106,3 @@ ax.set(xlabel='ambient temperature [°C]', ylabel='eta_collector',
 ax.grid()
 ax.legend()
 plt.show()
-plt.savefig('results/compare_temp_dependency_method2.png')
