16398831ffd9bc7bcb2bd20acc017ff301340e17
Jakob Wolf <jakob.wolf@beuth-hochschule.de>
2020-06-04 09:57:15 +0200

    Make flake8 happy

--- tests/test_absorption_chillers.py
+++ tests/test_absorption_chillers.py
@@ -94,7 +94,7 @@ def test_raised_exception_argument_length_01():
     with pytest.raises(ValueError):
         ac.calc_characteristic_temp(
             t_hot=[85],
-            t_cool=[30]*2,
+            t_cool=[30] * 2,
             t_chill=[15],
             coef_a=2.5,
             coef_e=1.8,
@@ -105,9 +105,9 @@ def test_raised_exception_argument_length_02():
     """Test if an exception is raised if input argument is too short."""
     with pytest.raises(ValueError):
         ac.calc_characteristic_temp(
-            t_hot=[85]*3,
-            t_cool=[30]*2,
-            t_chill=[15]*3,
+            t_hot=[85] * 3,
+            t_cool=[30] * 2,
+            t_chill=[15] * 3,
             coef_a=2.5,
             coef_e=1.8,
             method='kuehn_and_ziegler')
