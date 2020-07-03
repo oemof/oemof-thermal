a156a58256c0784ecd2b5eebe7f9486a8ea3e86a
Marie Gering <marie-claire.gering@rl-institut.de>
2020-07-03 17:18:32 +0200

    Fix results path for solar thermal collector

--- examples/solar_thermal_collector/flat_plate_collector_investment.py
+++ examples/solar_thermal_collector/flat_plate_collector_investment.py
@@ -174,7 +174,7 @@ solar_bus = solph.views.node(results, 'solar')['sequences']
 df = pd.merge(
     pd.merge(electricity_bus, thermal_bus, left_index=True, right_index=True),
     solar_bus, left_index=True, right_index=True)
-df.to_csv(results_path + 'flat_plate_results.csv')
+df.to_csv(os.path.join(results_path, 'flat_plate_results.csv'))
 
 # Example plot
 plot_collector_heat(precalc_data, periods, eta_0)
