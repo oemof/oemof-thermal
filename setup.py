2e8ac3aa82518d2d454e74b4e93b82a1865da4d4
caroline.moeller <caroline.moeller@rl-institut.de>
2020-10-14 10:51:56 +0200

    Set version number back to release version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 setup(name='oemof.thermal',
-      version='0.0.4rc1',
+      version='0.0.4',
       author='oemof developer group',
       author_email='contact@oemof.org',
       description=(
