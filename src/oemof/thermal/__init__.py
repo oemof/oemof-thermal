905a654aed6ba5314b4e420c1714ce9ff62c6081
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-02-11 14:05:10 +0100

    Name submodules in init
    For better compatibility, submodules shoud be named in the init.
    Otherwise, the following is not possible:
        from oemof import thermal
        thermal.stratified_thermal_storage.calculate_losses(...)

--- src/oemof/thermal/__init__.py
+++ src/oemof/thermal/__init__.py
@@ -1,2 +1,11 @@
 __version__ = '0.0.5dev'
 __project__ = 'oemof.thermal'
+
+from . import (absorption_heatpumps_and_chillers,
+               compression_heatpumps_and_chillers,
+               facades,
+               stratified_thermal_storage,
+               cogeneration,
+               concentrating_solar_power,
+               solar_thermal_collector)
+
