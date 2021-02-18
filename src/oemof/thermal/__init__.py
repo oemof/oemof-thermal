ed01abc404ffd4675b2a0b0d02e75e518cfbab5b
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2021-02-18 19:18:13 +0100

    Ignore F401

--- src/oemof/thermal/__init__.py
+++ src/oemof/thermal/__init__.py
@@ -1,11 +1,10 @@
 __version__ = '0.0.5dev'
 __project__ = 'oemof.thermal'
 
-from . import (absorption_heatpumps_and_chillers,
-               compression_heatpumps_and_chillers,
-               facades,
-               stratified_thermal_storage,
-               cogeneration,
-               concentrating_solar_power,
-               solar_thermal_collector)
-
+from . import absorption_heatpumps_and_chillers  # noqa: F401
+from . import compression_heatpumps_and_chillers  # noqa: F401
+from . import facades  # noqa: F401
+from . import stratified_thermal_storage  # noqa: F401
+from . import cogeneration  # noqa: F401
+from . import concentrating_solar_power  # noqa: F401
+from . import solar_thermal_collector  # noqa: F401
