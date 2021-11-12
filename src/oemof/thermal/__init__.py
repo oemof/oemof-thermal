8e7a787b3e4fc77406f24debff5afc08113d2bfd
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-11-12 17:53:42 +0100

    Explicitly define public API

--- src/oemof/thermal/__init__.py
+++ src/oemof/thermal/__init__.py
@@ -1,10 +1,20 @@
 __version__ = '0.0.5'
 __project__ = 'oemof.thermal'
 
-from . import absorption_heatpumps_and_chillers  # noqa: F401
-from . import compression_heatpumps_and_chillers  # noqa: F401
-from . import facades  # noqa: F401
-from . import stratified_thermal_storage  # noqa: F401
-from . import cogeneration  # noqa: F401
-from . import concentrating_solar_power  # noqa: F401
-from . import solar_thermal_collector  # noqa: F401
+from . import absorption_heatpumps_and_chillers
+from . import compression_heatpumps_and_chillers
+from . import facades
+from . import stratified_thermal_storage
+from . import cogeneration
+from . import concentrating_solar_power
+from . import solar_thermal_collector
+
+__all__ = [
+    "absorption_heatpumps_and_chillers",
+    "compression_heatpumps_and_chillers",
+    "facades",
+    "stratified_thermal_storage",
+    "cogeneration",
+    "concentrating_solar_power",
+    "solar_thermal_collector",
+]
