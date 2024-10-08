95890b3fd50418fe9f2fa81051dd4285905fda5b
Patrik Schönfeldt <patrik@proxima.snft.de>
2024-10-08 21:29:19 +0200

    Adhere to Black

--- docs/conf.py
+++ docs/conf.py
@@ -256,7 +256,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ("index", "oemof", "oemof.thermal documentation", ["oemof developer group"], 1)
+    (
+        "index",
+        "oemof",
+        "oemof.thermal documentation",
+        ["oemof developer group"],
+        1,
+    )
 ]
 
 # If true, show URL addresses after external links.
